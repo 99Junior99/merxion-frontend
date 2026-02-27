c565a2d5f2946ad8708107ea68f464ac0f557cbc
matheus albuquer <mj898926@gmail.com>
2026-02-27 19:59:14 -0300

    atualização

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import "./Navbar.css"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <NavLink to='/' end>
-                <img src="/src/assets/logo-A2hPLwK0.png" alt="" />
-            </NavLink>
-            <div className='list-left'>
-                <a id=''>Funcionalidades</a>
-                <a id=''>Como funciona</a></div>
+  return (
+    <nav>
+      <NavLink to="/" end>
+        <img src="/src/assets/LOGO MERXION.jpeg" alt="" />
+      </NavLink>
+      <div className="list-left">
+        <a id="">Funcionalidades</a>
+        <a id="">Como funciona</a>
+      </div>
 
-            <div className='list-right'>
-                <NavLink className='cad-login' to='/login' end >Entrar</NavLink>
-                <NavLink className="cad-btn" to='/cadastro' end>Começar grátis</NavLink>
-            </div>
+      <div className="list-right">
+        <NavLink className="cad-login" to="/login" end>
+          Entrar
+        </NavLink>
+        <NavLink className="cad-btn" to="/cadastro" end>
+          Começar grátis
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
 
-
-        </nav>
-    )
-}
-
-export default Navbar
+export default Navbar;
